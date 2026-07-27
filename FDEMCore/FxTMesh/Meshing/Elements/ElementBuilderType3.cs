7ed46b04d08f87c2739a4885a28bf0b764fd7d5e
Stapleton, Scott E <Scott_Stapleton@uml.edu>
2026-07-27 16:53:09 -0400

    Specified the number of integration points for all elements

--- FDEMCore/FxTMesh/Meshing/Elements/ElementBuilderType3.cs
+++ FDEMCore/FxTMesh/Meshing/Elements/ElementBuilderType3.cs
@@ -10,7 +10,7 @@ namespace FDEMCore.FxTMesh.Meshing.Elements
         public override ElementBuildResult BuildMatrixQuad(Point2D[] fiber1Nodes, Point2D[] fiber2Nodes, bool isEdgeCCW)
         {
             var nodes = BuildMatrixQuad9(fiber1Nodes, fiber2Nodes, isEdgeCCW);
-            return MatrixQuad("2DQ9", nodes);
+            return MatrixQuad("2DQ9.4", nodes);
         }
     }
 }
